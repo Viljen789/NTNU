dabf0abe9a54db4534189b9d7c1447fbcc0c153d
Viljen Vassbø <viljen.vassbo@gmail.com>
2024-10-23 12:30:09 +0200

    ITGK øving 6

---
+++ ITGK/Øving 6/streng.py
@@ -0,0 +1,17 @@
+str1 = "iS"
+str1 = str1.lower()
+str2 = "Is this the real life? Is this just fantasy?"
+str2 = str2.lower()
+str3 = "cool"
+
+i = len(str1)
+while(i<len(str2)):
+    if(str1==str2[i-len(str1):i]):
+        str2=str2[:i-len(str1)] + str3 + str2[i:]
+    i+=1
+        
+
+        
+
+print(str2)
+
